4e4c64d0ec62faa164b29354ca24d1a51f34f42e
nh-dev26 <astroneer913@gmail.com>
2025-10-31 11:02:54 +0900

    aht25ok

--- hardware/aht25_reader.py
+++ hardware/aht25_reader.py
@@ -1,43 +1,26 @@
 import smbus2
 import time
 
-# I2C設定
 i2c = smbus2.SMBus(1)
 address = 0x38
 
-#トリガ設定コマンド
-set = [0xAC, 0x33, 0x00]
+i2c.write_i2c_block_data(address, 0xBE, [0x08, 0x00])
+time.sleep(0.2)
 
-#データ読み込み用
-dat = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+i2c.write_i2c_block_data(address, 0xAC, [0x33, 0x00])
 
-# DHT20/AHT25設定
-time.sleep(0.1)
-ret = i2c.read_byte_data(address, 0x71)
+time.sleep(0.5)  
+try:
+    data = i2c.read_i2c_block_data(address, 0x00, 6)
+except OSError as e:
+    if e.errno == 121:
 
-# if ret != 0x18:
-    # initialization process
+        time.sleep(0.1)
+        data = i2c.read_i2c_block_data(address, 0x00, 6)
 
+hum = ((data[1] << 12) | (data[2] << 4) | (data[3] >> 4)) * 100 / 1048576.0
+temp = (((data[3] & 0x0F) << 16) | (data[4] << 8) | data[5]) * 200 / 1048576.0 - 50
 
-# トリガ測定コマンド送信
-time.sleep(0.01)
-i2c.write_i2c_block_data(address, 0x00, set)
+print(f"Humidity: {hum:.2f} %")
+print(f"Temperature: {temp:.2f} c")
 
-# データの読み込み
-time.sleep(0.08)
-dat = i2c.read_i2c_block_data(address, 0x00, 0x07)
-
-# データ変換
-hum = dat[1] << 12 | dat[2] << 4 | ((dat[3] & 0xF0) >> 4)
-tmp = ((dat[3] & 0x0F) << 16) | dat[4] << 8 | dat[5]
-
-# 物理量変換
-hum = hum / 2**20 * 100
-tmp = tmp / 2**20 * 200 - 50
-
-# 表示
-print("hum: " + str(hum))
-print("tmp: " + str(tmp))
-    
-    
-    
